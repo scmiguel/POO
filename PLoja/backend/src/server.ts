bc910c6bff555f2f5c67203f1390173f728f3cc3
scmiguel <70957634+scmiguel@users.noreply.github.com>
2023-04-18 13:58:25 +0000

    18-04

--- PLoja/backend/src/server.ts
+++ PLoja/backend/src/server.ts
@@ -1,7 +1,15 @@
 import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+
+const prisma = new PrismaClient()
 const app = Fastify()
 app.get(`/hello`, () => {
 	return 'Hello World'
+})
+app.get(`/products`, async () => {
+    const habits = await prisma.product.findMany()
+    return habits
 })
 app.listen({
     port: 3333,
